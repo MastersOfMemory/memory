0d17dba6c6fd9b85ac7a487b3bc282b9b26ac2e4
MarcoGiesing <54842984+MarcoGiesing@users.noreply.github.com>
2019-10-31 13:55:16 +0100

    Add files via upload
    Updated code voor de Timer, hoeft alleen nog code toe te worden gevoegd om de kaarten om te draaien.

--- Timer/Timer/MainWindow.xaml.cs
+++ Timer/Timer/MainWindow.xaml.cs
@@ -24,34 +24,58 @@ namespace Timer
         public MainWindow()
         {
             InitializeComponent();
-
-
-
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             DispatcherTimer dt = new DispatcherTimer(); //Maakt de timer aan
-            dt.Interval = TimeSpan.FromSeconds(1); //Aantal waar de timer mee op telt
-            dt.Tick += dtTicker; //Verwijst naar de code in vanaf lijn 40
-            dt.Start(); //Start de timer zo gauw het programma runned
+            dt.Interval = TimeSpan.FromSeconds(1); //Aantal seconden waar de timer 1 af telt (Bij 5 zou het 5 seconden duren voor hij van 3 naar 2 gaat)
+            dt.Tick += dtClock; // Code vanaf line 40
+            dt.Start(); // Hier door start de timer
+            
+            
         }
 
         // Deze int is de tijd waar de timer mee begint
         private int TimerTime = 11;
-        private void dtTicker(object sender, EventArgs e)
+
+
+        private void dtClock(object sender, EventArgs e)
         {
             TimerTime--;
 
             Timer.Content = TimerTime.ToString();
 
-            //Bij 0 reset de timer naar 10
+            //Dit is het stuk waar de code af telt.
 
-            if (TimerTime == 0) 
+            if (TimerTime > 0)
             {
-                TimerTime = 11;
+                Timer.Content = "Je hebt nog " + TimerTime + " seconden om je zet te doen!";
+                //Add code voor kaart omdraaien
             }
+
+            //Als de timer 0 hit is de beurt van de speler voorbij. De timer word ook gereset naar 15
+            if (TimerTime == 0)
+            {
+                Timer.Content = "10 seconden zijn voorbij, de volgende speler is aan de beurt.";
+                TimerTime = 15;
+                //Add code voor het niet omdraaien van kaarten
+            }
+
+            //Van 15-10 word dit nog gedisplayed en daarna begint de timer weer met aftellen
+            if (TimerTime > 10)
+            {
+                Timer.Content = "10 seconden zijn voorbij, de volgende speler is aan de beurt.";
+                //Add code voor het niet omdraaien van kaarten
+            }
+
         }
 
+
+        //Dit zou de button voor de volgende zet moeten zijn
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
